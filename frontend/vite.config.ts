9accc89a0129f0790ec8b5ab2e697e3614d8fe9f
casualmeow <gleb.snurnikov@gmail.com>
2025-03-21 03:58:47 +0000

    routing rule update (added * as path)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
+      "@": path.resolve(__dirname, "./src"),
       "entities": path.resolve(__dirname, "./src/entities"),
       "pages": path.resolve(__dirname, "./src/pages"),
       "shared": path.resolve(__dirname, "./src/shared"),
